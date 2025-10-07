d8d5b3a32566345a548f1fbb25841632efb7ae9f
ibnabdullah1 <arfathossen541@gmail.com>
2025-10-07 18:45:43 +0600

    Add initial files for Introduction to C Programming

---
+++ index.c
@@ -0,0 +1,19 @@
+#include <stdio.h>  // Include Standard Input/Output header file
+
+/*
+ * Program: Basic C Program Example
+ * Description:
+ * This program prints a simple message to the console.
+ * It demonstrates the structure of a basic C program
+ * including header inclusion, main function, and return statement.
+ */
+
+// The main function - program execution starts here
+int main()
+{
+    // Print a message to the screen
+    printf("Hi! I'm a C program.\n");
+
+    // Return 0 indicates successful program termination
+    return 0;
+}
